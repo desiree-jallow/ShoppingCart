96af9c0a3820bf21bcbc5f94c99647e776bebb36
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-03-10 12:48:25 -0600

    Initial Commit

---
+++ ShoppingCart/ShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartApp.swift
+//  ShoppingCart
+//
+//  Created by Desiree on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
